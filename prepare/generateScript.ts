4dd7b6686e0a1cdc431227b8f59b05ddf6cc33ba
Bastian <bastian.p@outlook.com>
2023-03-01 20:43:07 -0300

    window Updated

--- prepare/generateScript.ts
+++ prepare/generateScript.ts
@@ -3,11 +3,14 @@ import cookie from './cookie';
 import Evaluator from './Evaluator';
 import Variation from './Variation';
 import constants from '../config/constants';
-import type { evaluator, variation } from '../types/databaseObjects';
+import type {
+  campaignWithVariationsEvaluators,
+  evaluator,
+  variation
+} from '../types/databaseObjects';
 import getRandomFromArray from '../utils/getRandomFromArray';
 
-// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-window.AB = window.AB || {};
+window.AB = window.AB ?? {};
 
 window.AB.findCampaignThatContainsVariation = (
   idCampaign: number,
@@ -25,82 +28,91 @@ window.AB.findCampaignThatContainsVariation = (
   return undefined;
 };
 
-const runScript = async (): Promise<void> => {
-  if (window.AB?.campaigns?.length === 0) return;
-
-  const abTestCookie = cookie.get(constants.cookie.name);
-  const cookieExists = abTestCookie !== '';
-
-  if (cookieExists) {
-    const cookieData: {
-      idCampaign: number | any;
-      idVariation: number | any;
-    } = JSON.parse(abTestCookie);
-
-    const idCampaignInCookie = Number(cookieData.idCampaign);
-    const idVariationInCookie = Number(cookieData.idVariation);
-
-    if (
-      Number.isInteger(idCampaignInCookie) &&
-      Number.isInteger(idVariationInCookie)
-    ) {
-      const campaignData = window.AB.findCampaignThatContainsVariation(
-        idCampaignInCookie,
-        idVariationInCookie
-      );
-
-      if (campaignData !== undefined) {
-        const evaluators = campaignData.evaluators.map(
-          (evaluatorData) =>
-            new Evaluator(evaluatorData.idEvaluator, evaluatorData.javascript)
-        );
-        const variations = campaignData.variations.map(
-          (variationData) =>
-            new Variation(
-              variationData.idVariation,
-              campaignData.idCampaign,
-              variationData.html,
-              variationData.css,
-              variationData.javascript,
-              variationData.traffic
-            )
-        );
-        const campaign = new Campaign(
-          campaignData.idCampaign,
-          evaluators,
-          variations
-        );
-
-        campaign.getSpecificFunction(idVariationInCookie)();
-        return;
-      }
-    }
-  }
-
-  const campaignDataToRun = getRandomFromArray(window.AB.campaigns);
-
-  const evaluators = campaignDataToRun.evaluators.map(
+window.AB.constructCampaignToRun = (
+  campaignData: campaignWithVariationsEvaluators
+): Campaign => {
+  const evaluators = campaignData.evaluators.map(
     (evaluatorData: evaluator) =>
       new Evaluator(evaluatorData.idEvaluator, evaluatorData.javascript)
   );
-  const variations = campaignDataToRun.variations.map(
+  const variations = campaignData.variations.map(
     (variationData: variation) =>
       new Variation(
         variationData.idVariation,
-        campaignDataToRun.idCampaign,
+        campaignData.idCampaign,
         variationData.html,
         variationData.css,
         variationData.javascript,
         variationData.traffic
       )
   );
-  const campaignToRun = new Campaign(
-    campaignDataToRun.idCampaign,
+  const campaign = new Campaign(
+    campaignData.idCampaign,
     evaluators,
     variations
   );
 
-  campaignToRun.getRandomFunction()();
+  return campaign;
 };
 
-runScript();
+window.AB.cookieName = constants.cookie.name;
+
+window.AB.readCookie = () => {
+  let result = {};
+
+  if (window.AB?.cookieName === undefined) return result;
+
+  const abTestCookie = cookie.get(window.AB.cookieName);
+  const cookieExists = abTestCookie !== '';
+
+  if (cookieExists) {
+    let cookieData: {
+      idCampaign: number | any;
+      idVariation: number | any;
+    };
+
+    try {
+      cookieData = JSON.parse(abTestCookie);
+    } catch {
+      return {};
+    }
+
+    const idCampaign = Number(cookieData.idCampaign);
+    const idVariation = Number(cookieData.idVariation);
+
+    if (Number.isInteger(idCampaign)) result = { ...result, idCampaign };
+    if (Number.isInteger(idVariation)) result = { ...result, idVariation };
+  }
+
+  return result;
+};
+
+window.AB.runScript = async () => {
+  if (window.AB?.campaigns?.length === 0) return;
+  if (window.AB?.readCookie === undefined) return;
+  if (window.AB?.findCampaignThatContainsVariation === undefined) return;
+  if (window.AB?.constructCampaignToRun === undefined) return;
+  if (window.AB?.campaigns === undefined) return;
+
+  const { idCampaign, idVariation } = window.AB.readCookie();
+
+  if (idCampaign !== undefined && idVariation !== undefined) {
+    const campaignData = window.AB.findCampaignThatContainsVariation(
+      idCampaign,
+      idVariation
+    );
+
+    if (campaignData !== undefined) {
+      const campaign = window.AB.constructCampaignToRun(campaignData);
+
+      campaign.getSpecificFunction(idVariation)();
+      return;
+    }
+  }
+
+  const campaignData = getRandomFromArray(window.AB.campaigns);
+  const campaign = window.AB.constructCampaignToRun(campaignData);
+  campaign.getRandomFunction()();
+};
+
+window.AB.runScript();
--- types/window.d.ts
+++ types/window.d.ts
@@ -1,13 +1,28 @@
+import type Campaign from './Campaign';
 import type { campaignWithVariationsEvaluators } from '../types/databaseObjects';
 
 declare global {
   interface Window {
-    AB: {
-      campaigns: campaignWithVariationsEvaluators[];
-      findCampaignThatContainsVariation: (
+    AB?: {
+      campaigns?: campaignWithVariationsEvaluators[];
+
+      findCampaignThatContainsVariation?: (
         campaign: number,
         idVariation: number
       ) => campaignWithVariationsEvaluators | undefined;
+
+      constructCampaignToRun?: (
+        campaignData: campaignWithVariationsEvaluators
+      ) => Campaign;
+
+      cookieName?: string;
+
+      readCookie?: () => {
+        idCampaign?: number;
+        idVariation?: number;
+      };
+
+      runScript?: () => Promise<void>;
     };
   }
 }
