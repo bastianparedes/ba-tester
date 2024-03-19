5b8c33388a9e2b0a6248d7d164038d7a12ef16bc
Bastián <bparedesp@falabella.cl>
2024-03-18 21:13:03 -0300

    A punto de quitar las audiencias

--- src/app/audiences/page.tsx
+++ src/app/audiences/page.tsx
@@ -5,49 +5,56 @@ import React, { useEffect, useState } from 'react';
 import IndexComponents from './components';
 import { FiltersProvider } from './components/context/filters';
 import commonConstants from '../../../config/common/constants';
-import constants from '../../../config/constants';
+import { trpcClient } from '../../../lib/trpc/client';
+import type { AudienceWithDate } from '../../../types/databaseObjects';
+import { useLocalStorage } from '../../../utils/customHook';
 import Loader from '../common/Loader';
 
 const Index = () => {
-  const [audiences, setAudiences] = useState([]);
-  const [filterByStatusList, setFilterByStatusList] = useState<
-    (typeof commonConstants)['audienceStatus'][number][]
-  >([commonConstants.status.active]);
-  const [filterByname, setFilterByName] = useState('');
-  const [orderBy, setOrderBy] = useState('lastModifiedDate');
-  const [order, setOrder] = useState<'asc' | 'desc'>('desc');
-  const [quantity, setQuantity] = useState(15);
-  const [page, setPage] = useState(0);
-  const [count, setCount] = useState(0);
-  const [loading, setLoading] = useState(true);
+  const [audiences, setAudiences] = useState<AudienceWithDate[]>([]);
+  const [filterByStatusList, setFilterByStatusList] = useLocalStorage<
+    (typeof commonConstants)['campaignStatus'][number][]
+  >('filterByStatusList', [
+    commonConstants.status.inactive,
+    commonConstants.status.active
+  ]);
+  const [filterByname, setFilterByName] = useLocalStorage('filterByName', '');
+  const [orderBy, setOrderBy] = useLocalStorage<
+    'status' | 'name' | 'id' | 'lastModifiedDate'
+  >('orderBy', 'lastModifiedDate');
+  const [order, setOrder] = useLocalStorage<'asc' | 'desc'>('order', 'desc');
+  const [quantity, setQuantity] = useLocalStorage('quantity', 15);
+  const [page, setPage] = useLocalStorage('page', 0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [count, setCount] = useLocalStorage('count', 0);
+  const getAudiences = trpcClient.getAudiences.useMutation({
+    onSettled(data, error) {
+      if (error !== null) return;
+      if (data === undefined) return;
+      setAudiences(data.audiences);
+      setCount(data.count);
+    }
+  });
 
   useEffect(() => {
-    (async () => {
-      setLoading(true);
-      const response = await fetch(constants.api.audience.read, {
-        body: JSON.stringify({
-          args: {
-            name: filterByname.toLocaleLowerCase(),
-            order,
-            orderBy,
-            page,
-            quantity,
-            statusList: filterByStatusList
-          }
-        }),
-        method: 'POST'
-      });
-
-      const json = await response.json();
-      setAudiences(json.response.audiences);
-      setCount(json.response.count);
-      setLoading(false);
-    })();
+    getAudiences.mutate({
+      name: filterByname,
+      orderBy: orderBy,
+      orderDirection: order,
+      page: page,
+      quantity: quantity,
+      statusList: filterByStatusList
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [filterByStatusList, filterByname, order, orderBy, page, quantity]);
 
   useEffect(() => {
     setPage(0);
-  }, [filterByStatusList, filterByname, quantity]);
+  }, [filterByStatusList, filterByname, quantity, setPage]);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
 
   return (
     <FiltersProvider
@@ -65,7 +72,7 @@ const Index = () => {
       setPage={setPage}
       setQuantity={setQuantity}
     >
-      {loading && <Loader />}
+      {(getCampaigns.isLoading || isLoading) && <Loader />}
       <IndexComponents audiences={audiences} />
     </FiltersProvider>
   );
