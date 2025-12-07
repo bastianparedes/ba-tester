070ae01f821f8c0c5b2db212e1a8233dc822847c
Bastián <bparedesp@falabella.cl>
2025-12-07 10:58:27 -0300

    koijuyhiljnoñkl

--- src/app/api/public/script/route.ts
+++ src/app/api/public/script/route.ts
@@ -4,10 +4,10 @@ import { getCampaignsForScript } from '@/script/utils/database';
 
 const GET = async () => {
   const fileExists = fs.existsSync(path.join(process.cwd(), 'dist', 'script.js'));
-  if (!fileExists) return '';
+  if (!fileExists) return new Response('');
 
   const campaigns = await getCampaignsForScript();
-  if (campaigns.length === 0) return '';
+  if (campaigns.length === 0) return new Response('');
 
   const stringWindow = `window.ba_tester = window.ba_tester || {}\n;window.ba_tester.campaignsData = ${JSON.stringify(campaigns)};`;
   const script = fs.readFileSync(path.join(process.cwd(), 'dist', 'script.js'), 'utf-8');
