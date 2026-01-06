43744c2a6f29c95d67a0f1a82101f044cc15e9f1
Bastian <bastian.p@outlook.com>
2026-01-06 20:50:31 -0300

    xfdbxcf

--- src/libs/permissions.ts
+++ src/libs/permissions.ts
@@ -1,5 +1,21 @@
-const permissions = {
-  admin: ['read', 'write', 'delete'],
-  user: ['read', 'write'],
-  guest: ['read'],
-};
+import { flattenObject } from '@/utils/object';
+
+export const permissions = Object.freeze({
+  user: {
+    read: 'user.read',
+    write: 'user.write',
+    delete: 'user.delete',
+  },
+  tenant: {
+    read: 'tenant.read',
+    write: 'tenant.write',
+    delete: 'tenant.delete',
+  },
+  campaign: {
+    read: 'campaign.read',
+    write: 'campaign.write',
+    delete: 'campaign.delete',
+  },
+});
+
+export const flatPermissions = flattenObject(permissions);
--- src/utils/object.ts
+++ src/utils/object.ts
@@ -1,5 +1,35 @@
-const isObject = (object: unknown) => {
+export const isObject = (object: unknown) => {
   return Object.prototype.toString.call(object) === '[object Object]';
 };
 
-export { isObject };
+type NestedObject<T extends string | number | boolean> = {
+  [key: string]: T | NestedObject<T>;
+};
+export function flattenObject<T extends string | number | boolean>(obj: NestedObject<T>): T[] {
+  const result: T[] = [];
+
+  const keys = Object.keys(obj) as (keyof typeof obj)[];
+  keys.forEach((key) => {
+    const value = obj[key];
+    if (isObject(value)) {
+      const nestedValues = flattenObject(value as NestedObject<T>);
+      nestedValues.forEach((value) => result.push(value));
+    } else {
+      value
+    }
+  });
+
+  return result;
+
+  function recurse(value: string | NestedStringObject) {
+    if (typeof value === 'string') {
+      result.push(value);
+    } else {
+      for (const key in value) {
+        recurse(value[key]);
+      }
+    }
+  }
+  recurse(obj);
+  return result as FlattenValues<T>;
+}
